43b55bfeef916c2944135a11a488039376fcf39d
Jihan Aqilah Hartono <jihanaqilah@upi.edu>
2025-04-29 15:34:12 +0700

    commit

--- PROVIS-3-Rancangan-UI.git/lib/Main.dart
+++ PROVIS-3-Rancangan-UI.git/lib/Main.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Recipe App',
+      theme: ThemeData(
+        primaryColor: const Color(0xFF2A9D8F),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF2A9D8F),
+          primary: const Color(0xFF2A9D8F),
+        ),
+        fontFamily: 'Poppins',
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
