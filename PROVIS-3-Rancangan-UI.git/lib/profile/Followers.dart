50c03d02ff0d602e13e472d69c2591a64da01496
Jihan Aqilah Hartono <jihanaqilah@upi.edu>
2025-04-30 21:00:40 +0700

    commit

--- PROVIS-3-Rancangan-UI.git/lib/profile/Followers.dart
+++ PROVIS-3-Rancangan-UI.git/lib/profile/Followers.dart
@@ -1,33 +1,40 @@
 import 'package:flutter/material.dart';
 
-class FollowingPage extends StatefulWidget {
-  const FollowingPage({Key? key}) : super(key: key);
+class FollowersPage extends StatefulWidget {
+  const FollowersPage({Key? key}) : super(key: key);
 
   @override
-  State<FollowingPage> createState() => _FollowingPageState();
+  State<FollowersPage> createState() => _FollowersPageState();
 }
 
-class _FollowingPageState extends State<FollowingPage> {
+class _FollowersPageState extends State<FollowersPage> {
   final TextEditingController _searchController = TextEditingController();
+  int? _highlightedIndex;
   
-  // Sample data for following users
-  final List<Map<String, dynamic>> following = [
+  // Sample data for followers
+  final List<Map<String, dynamic>> followers = [
     {
-      'username': '@noel_dsp',
-      'name': 'Manuel Simbolon',
-      'image': 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e',
-      'isFollowing': true,
-    },
-    {
-      'username': '@chef_manz',
-      'name': 'Emily Wangui',
+      'username': '@nancymasak',
+      'name': 'Nancy Joy',
       'image': 'https://images.unsplash.com/photo-1494790108377-be9c29b29330',
       'isFollowing': true,
     },
     {
-      'username': '@asifavegan',
-      'name': 'Asfa Sababi',
+      'username': '@chefwakil',
+      'name': 'Sheyla',
       'image': 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80',
+      'isFollowing': true,
+    },
+    {
+      'username': '@ricecook',
+      'name': 'Erin Mandela',
+      'image': 'https://images.unsplash.com/photo-1534528741775-53994a69daeb',
+      'isFollowing': true,
+    },
+    {
+      'username': '@clairine',
+      'name': 'Claire Akinyi',
+      'image': 'https://images.unsplash.com/photo-1531123897727-8f129e1688ce',
       'isFollowing': true,
     },
     {
@@ -35,10 +42,11 @@ class _FollowingPageState extends State<FollowingPage> {
       'name': 'Yathy',
       'image': 'https://images.unsplash.com/photo-1544005313-94ddf0286df2',
       'isFollowing': true,
+      'highlighted': true,
     },
     {
       'username': '@karikou',
-      'name': 'Kari Raissidou',
+      'name': 'Kari Reid',
       'image': 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d',
       'isFollowing': true,
     },
@@ -49,24 +57,30 @@ class _FollowingPageState extends State<FollowingPage> {
       'isFollowing': true,
     },
     {
-      'username': '@speedchef',
-      'name': 'Speed sisuu',
+      'username': '@tastymorocco',
+      'name': 'Moga Eristing Atoy',
       'image': 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d',
       'isFollowing': true,
     },
     {
-      'username': '@flavourswithflavian',
-      'name': 'Flavian',
+      'username': '@spicyfantasy',
+      'name': 'Spicy Fantasy',
       'image': 'https://images.unsplash.com/photo-1534528741775-53994a69daeb',
       'isFollowing': true,
     },
-    {
-      'username': '@rawfood',
-      'name': 'Raw Food',
-      'image': 'https://images.unsplash.com/photo-1531123897727-8f129e1688ce',
-      'isFollowing': true,
-    },
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    // Find the highlighted index
+    for (int i = 0; i < followers.length; i++) {
+      if (followers[i]['highlighted'] == true) {
+        _highlightedIndex = i;
+        break;
+      }
+    }
+  }
 
   @override
   void dispose() {
@@ -83,7 +97,7 @@ class _FollowingPageState extends State<FollowingPage> {
         children: [
           _buildSearchBar(),
           Expanded(
-            child: _buildFollowingList(),
+            child: _buildFollowersList(),
           ),
         ],
       ),
@@ -113,6 +127,23 @@ class _FollowingPageState extends State<FollowingPage> {
           const SizedBox(height: 4),
           Row(
             children: [
+              GestureDetector(
+                onTap: () {
+                  // Navigate to following page
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const FollowingPage()),
+                  );
+                },
+                child: const Text(
+                  '120 Following',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16),
               Container(
                 decoration: const BoxDecoration(
                   border: Border(
@@ -124,28 +155,11 @@ class _FollowingPageState extends State<FollowingPage> {
                 ),
                 padding: const EdgeInsets.only(bottom: 4),
                 child: const Text(
-                  '120 Following',
+                  '250 Followers',
                   style: TextStyle(
                     color: Color(0xFF2A9D8F),
                     fontSize: 12,
                     fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ),
-              const SizedBox(width: 16),
-              GestureDetector(
-                onTap: () {
-                  // Navigate to followers page
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => const FollowersPage()),
-                  );
-                },
-                child: const Text(
-                  '250 Followers',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 12,
                   ),
                 ),
               ),
@@ -176,19 +190,25 @@ class _FollowingPageState extends State<FollowingPage> {
     );
   }
 
-  Widget _buildFollowingList() {
+  Widget _buildFollowersList() {
     return ListView.builder(
-      itemCount: following.length,
+      itemCount: followers.length,
       padding: const EdgeInsets.symmetric(vertical: 8),
       itemBuilder: (context, index) {
-        final user = following[index];
-        return _buildFollowingItem(user);
+        final user = followers[index];
+        return _buildFollowerItem(user, index);
       },
     );
   }
 
-  Widget _buildFollowingItem(Map<String, dynamic> user) {
-    return Padding(
+  Widget _buildFollowerItem(Map<String, dynamic> user, int index) {
+    final bool isHighlighted = index == _highlightedIndex;
+    
+    return Container(
+      decoration: BoxDecoration(
+        color: isHighlighted ? const Color(0xFF9C27B0).withOpacity(0.2) : Colors.transparent,
+        borderRadius: BorderRadius.circular(8),
+      ),
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       child: Row(
         children: [
@@ -203,8 +223,8 @@ class _FollowingPageState extends State<FollowingPage> {
               children: [
                 Text(
                   user['username'],
-                  style: const TextStyle(
-                    color: Color(0xFF2A9D8F),
+                  style: TextStyle(
+                    color: isHighlighted ? const Color(0xFF9C27B0) : const Color(0xFF2A9D8F),
                     fontWeight: FontWeight.bold,
                     fontSize: 14,
                   ),
@@ -222,23 +242,36 @@ class _FollowingPageState extends State<FollowingPage> {
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
             decoration: BoxDecoration(
-              color: const Color(0xFFAFDED9),
+              color: Colors.transparent,
+              border: Border.all(
+                color: isHighlighted ? const Color(0xFF9C27B0) : const Color(0xFF2A9D8F),
+              ),
               borderRadius: BorderRadius.circular(20),
             ),
-            child: const Text(
+            child: Text(
               'Following',
               style: TextStyle(
-                color: Color(0xFF2A9D8F),
+                color: isHighlighted ? const Color(0xFF9C27B0) : const Color(0xFF2A9D8F),
                 fontWeight: FontWeight.bold,
                 fontSize: 12,
               ),
             ),
           ),
-          IconButton(
-            icon: const Icon(Icons.more_vert, color: Colors.grey),
-            onPressed: () {
-              // Show options menu
-            },
+          const SizedBox(width: 8),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+            decoration: BoxDecoration(
+              color: isHighlighted ? const Color(0xFF9C27B0).withOpacity(0.2) : const Color(0xFFAFDED9),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Text(
+              'Delete',
+              style: TextStyle(
+                color: isHighlighted ? const Color(0xFF9C27B0) : const Color(0xFF2A9D8F),
+                fontWeight: FontWeight.bold,
+                fontSize: 12,
+              ),
+            ),
           ),
         ],
       ),
@@ -269,8 +302,8 @@ class _FollowingPageState extends State<FollowingPage> {
 }
 
 // This is just a stub to make the code compile
-class FollowersPage extends StatelessWidget {
-  const FollowersPage({Key? key}) : super(key: key);
+class FollowingPage extends StatelessWidget {
+  const FollowingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
